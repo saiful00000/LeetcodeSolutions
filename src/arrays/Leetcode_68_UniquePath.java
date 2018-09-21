e1936c798b96b27c61a23901b2ada7602009df3b
saiful00000 <>
2018-09-21 17:16:59 +0600

    68 unique path solved

---
+++ src/arrays/Leetcode_68_UniquePath.java
@@ -0,0 +1,20 @@
+package arrays;
+
+public class Leetcode_68_UniquePath {
+
+    public int uniquePath(int m, int n) {
+        int[][] paths = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+                    paths[i][j] = 1;
+                } else {
+                    paths[i][j] = paths[i-1][j] + paths[i][j-1];
+                }
+            }
+        }
+        return paths[m-1][n-1];
+    }
+
+}
