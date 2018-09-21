92ec6bc7aefd82ec86afb48aa2be2689856b9a23
saiful00000 <>
2018-09-21 17:14:34 +0600

    229 mejority element two solved

--- src/TestCLass.java
+++ src/TestCLass.java
@@ -1,15 +1,32 @@
 
+
 public class TestCLass {
 
-    public static void main(String[] args) {
-
-        int n = 218;
-        while (n > 2) {
-            n = n/2;
+    public static int uniquePaths(int m, int n) {
+        int[][] grid = new int[m][n];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(i==0||j==0)
+                    grid[i][j] = 1;
+                else
+                    grid[i][j] = grid[i][j-1] + grid[i-1][j];
+            }
         }
-        if(n == 2)
-            System.out.println("yes");
-        else
-            System.out.println("no");
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(grid[i][j] +" ");
+            }
+            System.out.println();
+        }
+
+        return grid[m-1][n-1];
     }
+
+    public static void main(String[] args) {
+        int m = 7;
+        int n = 3;
+        System.out.println(uniquePaths(m,n));
+    }
+
 }
---
+++ src/arrays/Leetcode_229_MajorityElemetTwo.java
@@ -0,0 +1,19 @@
+package arrays;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Leetcode_229_MajorityElemetTwo {
+
+    public static List<Integer> majorityElement(int[] nums) {
+
+
+        return new LinkedList<>();
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {1,1,1,3,3,2,2,2};
+        System.out.println(majorityElement(a));
+    }
+}
