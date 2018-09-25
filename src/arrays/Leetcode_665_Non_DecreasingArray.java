e7fc5516c8ee3c9829f2e1f5e07f2181bba893a9
saiful00000 <>
2018-09-25 19:39:33 +0600

    665

---
+++ src/arrays/Leetcode_665_Non_DecreasingArray.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Leetcode_665_Non_DecreasingArray {
+    public static boolean checkPossibility(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                if (i > 0) {
+                    if (nums[i-1] <= nums[i+1]) nums[i] = nums[i-1];
+                    else nums[i+1] = nums[i];
+                }
+                count++;
+            }
+        }
+        return count <= 1;
+    }
+}
