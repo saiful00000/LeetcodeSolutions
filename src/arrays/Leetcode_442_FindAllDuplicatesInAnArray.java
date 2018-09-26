6726c673cb360cb6574bb8e947c511bc3accb779
saiful00000 <>
2018-09-27 01:29:36 +0600

    array 442

---
+++ src/arrays/Leetcode_442_FindAllDuplicatesInAnArray.java
@@ -0,0 +1,19 @@
+package arrays;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode_442_FindAllDuplicatesInAnArray {
+
+    public static List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                list.add(nums[i]);
+            }
+        }
+        return list;
+    }
+}
