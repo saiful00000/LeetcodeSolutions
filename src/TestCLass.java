7e8e5a228f93dd8be0c9de618b767ef1d9bbf5a6
saiful00000 <>
2018-09-27 13:16:21 +0600

    array 560

--- src/TestCLass.java
+++ src/TestCLass.java
@@ -1,17 +1,20 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class TestCLass {
 
     public static void main(String[] args) {
-        int[] nums = {0,1,1,3,3};
-        int k = 4;
-        int n = nums.length;
-        for (int i = 0; i <= n-k; i++) {
-            for (int j = i; j < i+k; j++) {
-                System.out.print(nums[j] +" ");
-            }
-            System.out.println();
+        List<String> list = new ArrayList<>();
+        list.add("s");
+        list.add("a");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
         }
+        if (list.contains("s")) {
+            list.remove("a");
+        }
+        System.out.println(list);
     }
 }
