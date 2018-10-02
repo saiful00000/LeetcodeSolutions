43595fec1436388de0bb936266a835c5942cd13c
saiful00000 <>
2018-10-03 02:48:52 +0600

    solved using additional array

--- src/maths/Leetcode_204_CountPrime.java
+++ src/maths/Leetcode_204_CountPrime.java
@@ -3,41 +3,18 @@ package maths;
 public class Leetcode_204_CountPrime {
 
     public static int countPrimes(int n) {
+
+        boolean[] dp = new boolean[n];
         int count = 0;
 
-        if (n < 2) return count;
-        if (n == 2) return count;
-
-        count = 1;
-        for (int i = 2; i <=n ; i++) {
-
-            boolean flag = true;
-            if (i % 2 != 0) {
-                for (int j = 3; j < i / 2; j += 2) {
-                    if (i % j == 0) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) count++;
-
-            } else {
-                for (int j = 3; j < i/2; j++) {
-                    if (i % j == 0) {
-                        flag = false;
-                        break;
-                    }
-                }
-                if (flag) count++;
+        for (int i = 2; i < n; i++) {
+            if (dp[i]) continue;
+            count++;
+            for (int j = i; j < n; j = j+i) {
+                dp[j] = true;
             }
         }
 
-
         return count;
-    }
-
-    public static void main(String[] args) {
-        int a = 10;
-        System.out.println(countPrimes(a));
     }
 }
