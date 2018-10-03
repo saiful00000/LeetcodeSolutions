ad1a6938f1d59ed87f7a90555c7951a7f3a80a04
saiful00000 <>
2018-10-03 16:10:14 +0600

    dynamic 198

---
+++ src/DynamicProgramming/Leetcode_198_HouseRobber.java
@@ -0,0 +1,16 @@
+package DynamicProgramming;
+
+public class Leetcode_198_HouseRobber {
+
+    public int rob(int[] nums) {
+        if (nums.length == 0 || nums == null) return 0;
+        if (nums.length == 1) return nums[0];
+
+        nums[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; i++) {
+            nums[i] = Math.max( nums[i] + nums[i-2] , nums[i-1] );
+        }
+        return nums[nums.length-1];
+    }
+}
