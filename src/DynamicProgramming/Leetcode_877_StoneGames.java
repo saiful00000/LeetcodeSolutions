d21b82ccc81dff7a1b29dce9b460cf8e12de95aa
saiful00000 <>
2018-10-07 19:30:05 +0600

    dynamic 877

--- src/DynamicProgramming/Leetcode_877_StoneGames.java
+++ src/DynamicProgramming/Leetcode_877_StoneGames.java
@@ -2,7 +2,7 @@ package DynamicProgramming;
 
 public class Leetcode_877_StoneGames {
 
-    public boolean stoneGame(int[] piles) {
+    public  boolean stoneGame(int[] piles) {
         return true;
     }
 }
