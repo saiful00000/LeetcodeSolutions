fc8ed04554741ad00c82dd766f9c89aa9a96fe1b
saiful00000 <>
2018-10-16 23:27:24 +0600

    done and test the code

---
+++ src/DynamicProgramming/Leetcode_647_PelindromicSubstring.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+public class Leetcode_647_PelindromicSubstring {
+    int count = 1;
+    public int countSubstrings(String s) {
+        int n = s.length();
+        if (n < 1) return 0;
+
+        for (int i = 0; i < n-1; i++) {
+            countSub(s, i, i);
+            countSub(s, i,i+1);
+        }
+        return count;
+    }
+
+    public void countSub(String s, int i, int j) {
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            count++;
+            i--;
+            j++;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "aaa";
+        System.out.println(new Leetcode_647_PelindromicSubstring().countSubstrings(s));
+    }
+}
