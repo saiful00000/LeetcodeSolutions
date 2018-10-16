61fe6b9681efc466c54847ff200eee187ccc85e6
saiful00000 <>
2018-10-17 02:09:15 +0600

    required method implemented

--- src/DynamicProgramming/Leetcode_413_ArithmeticSlices.java
+++ src/DynamicProgramming/Leetcode_413_ArithmeticSlices.java
@@ -3,6 +3,16 @@ package DynamicProgramming;
 public class Leetcode_413_ArithmeticSlices {
 
     public int numberOfArithmeticSlices(int[] nums) {
-
+        int dp = 0;
+        int sum = 0;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                dp += 1;
+                sum += dp;
+            } else {
+                dp = 0;
+            }
+        }
+        return dp;
     }
 }
