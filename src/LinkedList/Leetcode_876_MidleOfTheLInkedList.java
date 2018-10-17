65b3d9a7bfc7837671de603de712dd40fd4ffd68
saiful00000 <>
2018-10-17 22:32:13 +0600

    accepted

--- src/LinkedList/Leetcode_876_MidleOfTheLInkedList.java
+++ src/LinkedList/Leetcode_876_MidleOfTheLInkedList.java
@@ -1,5 +1,22 @@
 package LinkedList;
 
 public class Leetcode_876_MidleOfTheLInkedList {
+    /*class ListNode{
+        int val;
+        ListNode next;
 
+        ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }*/
+
+    public ListNode middleNode(ListNode head) {
+        ListNode midle = head, end = head;
+        while (end.next != null) {
+            midle = midle.next;
+            end = end.next.next;
+        }
+        return midle;
+    }
 }
