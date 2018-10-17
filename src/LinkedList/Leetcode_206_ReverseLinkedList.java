60b2adb237a172029e3ff47dc1e27b5ce65705ee
saiful00000 <>
2018-10-18 00:12:04 +0600

    accepted

---
+++ src/LinkedList/Leetcode_206_ReverseLinkedList.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class Leetcode_206_ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return head = prev;
+    }
+}
