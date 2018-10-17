6f234e711232fe34555d37c34783ee8e95620eda
saiful00000 <>
2018-10-17 22:21:46 +0600

    solved

--- src/LinkedList/Leetcode_83_RemoveDuplicateFromSOrtedList.java
+++ src/LinkedList/Leetcode_83_RemoveDuplicateFromSOrtedList.java
@@ -2,4 +2,25 @@ package LinkedList;
 
 public class Leetcode_83_RemoveDuplicateFromSOrtedList {
 
+    class ListNode{
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            next = null;
+        }
+    }
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node = head;
+        while (node != null || node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            }else{
+                node = node.next;
+            }
+        }
+        return head;
+    }
 }
