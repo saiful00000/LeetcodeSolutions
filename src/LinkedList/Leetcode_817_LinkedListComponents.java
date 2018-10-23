20e2db1cbe2aef24fa716d2d173db563507de187
saiful00000 <>
2018-10-23 22:47:08 +0600

    code in test

--- src/LinkedList/Leetcode_817_LinkedListComponents.java
+++ src/LinkedList/Leetcode_817_LinkedListComponents.java
@@ -1,5 +1,18 @@
 package LinkedList;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Leetcode_817_LinkedListComponents {
-    
+    public int numComponents(ListNode head, int[] G) {
+        Set<Integer> set = new HashSet<>();
+        int count = 0;
+        ListNode temp = head;
+        while (temp != null) {
+            if (set.contains(temp.val) && (temp.next == null || !set.contains(temp.next.val)))
+                count++;
+            temp = temp.next;
+        }
+        return count;
+    }
 }
