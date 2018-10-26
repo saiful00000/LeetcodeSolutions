f28c9e49062780706b494b115fc79b901be2a414
saiful00000 <>
2018-10-27 00:24:15 +0600

    accepted with beating 77% java solution :)

--- src/Stack/Leetcode_225_ImplementStackUsingQueue.java
+++ src/Stack/Leetcode_225_ImplementStackUsingQueue.java
@@ -1,5 +1,44 @@
 package Stack;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Leetcode_225_ImplementStackUsingQueue {
+
+    class MyStack{
+
+        Queue<Integer> q = new LinkedList<>();
+
+        /** Initialize your data structure here. */
+        public MyStack() {
+
+        }
+
+        /** Push element x onto stack. */
+        public void push(int x) {
+            q.add(x);
+            int n = q.size();
+            while(n>1){
+                q.add(q.remove());
+                n--;
+            }
+        }
+
+        /** Removes the element on top of the stack and returns that element. */
+        public int pop() {
+            return q.remove();
+        }
+
+        /** Get the top element. */
+        public int top() {
+            return q.peek();
+        }
+
+        /** Returns whether the stack is empty. */
+        public boolean empty() {
+            return q.isEmpty();
+        }
+    }
+
 
 }
