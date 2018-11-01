0fe8449a345ad857f6d4c7ada4e0df29041f6484
saiful00000 <>
2018-11-01 22:58:46 +0600

    accepted with 3ms runtime

--- src/Tree/Leetcode_100_Same_Tree.java
+++ src/Tree/Leetcode_100_Same_Tree.java
@@ -1,4 +1,15 @@
 package Tree;
 
 public class Leetcode_100_Same_Tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val == q.val) {
+            return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        } else {
+            return false;
+        }
+    }
 }
