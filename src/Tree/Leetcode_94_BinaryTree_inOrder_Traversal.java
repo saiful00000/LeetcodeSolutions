5f793b50256b20de886171e748ebcd2ed1ead507
saiful00000 <>
2018-11-01 22:33:16 +0600

    accepted with 1ms runtime

--- src/Tree/Leetcode_94_BinaryTree_inOrder_Traversal.java
+++ src/Tree/Leetcode_94_BinaryTree_inOrder_Traversal.java
@@ -1,5 +1,29 @@
 package Tree;
 
-public class Leetcode_94_BinaryTree_inOrder_Traversal {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
+public class Leetcode_94_BinaryTree_inOrder_Traversal {
+    public List<Integer> inorderTraversal(TreeNode root){
+        if (root == null) return new ArrayList<>();
+
+        List<Integer> list = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode temp = root;
+
+        while (stack.size() > 0 || temp != null) {
+
+            while (temp != null) {
+                stack.push(temp);
+                temp = temp.left;
+            }
+
+            temp = stack.pop();
+            list.add(temp.val);
+            temp = temp.right;
+        }
+
+        return list;
+    }
 }
