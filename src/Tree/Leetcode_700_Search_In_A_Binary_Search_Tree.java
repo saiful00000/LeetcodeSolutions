07736c3ab1bc23e0b29520aba7a75fe2a2411d83
saiful00000 <>
2018-11-06 21:23:15 +0600

    code simplefied

--- src/Tree/Leetcode_700_Search_In_A_Binary_Search_Tree.java
+++ src/Tree/Leetcode_700_Search_In_A_Binary_Search_Tree.java
@@ -2,16 +2,11 @@ package Tree;
 
 public class Leetcode_700_Search_In_A_Binary_Search_Tree {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) {
+        if (root == null || root.val == val)
             return root;
-        }
-        if (root.val == val) {
-            return root;
-        }
-        if (root.val > val) {
+        if (root.val > val)
             return searchBST(root.left, val);
-        } else {
+        else
             return searchBST(root.right, val);
-        }
     }
 }
