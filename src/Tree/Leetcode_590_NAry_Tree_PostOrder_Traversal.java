d362a088589fd1331c62562acbbf40581ceb39cd
saiful00000 <>
2018-11-06 20:24:06 +0600

    in coding

---
+++ src/Tree/Leetcode_590_NAry_Tree_PostOrder_Traversal.java
@@ -0,0 +1,18 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Leetcode_590_NAry_Tree_PostOrder_Traversal {
+    /*public List<Integer> postorder(Node root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        Stack<Node> stack = new Stack<Node>();
+        List<Integer> ans = new ArrayList<Integer>();
+
+        while ()
+    }*/
+}
