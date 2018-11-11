ebb8564a1916490cdc3f4d4d2082c03082e94354
saiful00000 <>
2018-11-12 02:40:33 +0600

    accepted with 0ms runtime and beat 100% java solution

---
+++ src/Tree/Leetcode_112_Path_Sum.java
@@ -0,0 +1,10 @@
+package Tree;
+
+public class Leetcode_112_Path_Sum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (sum - root.val == 0 && root.left == null && root.right == null)
+            return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
