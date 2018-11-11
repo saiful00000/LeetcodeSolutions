7d1722590525b38d8251831ca3d68eef4481dd63
saiful00000 <>
2018-11-12 02:19:08 +0600

    accepted with 6ms runtime

---
+++ src/Tree/Leetcode_814_BinaryTree_Pruning.java
@@ -0,0 +1,12 @@
+package Tree;
+
+public class Leetcode_814_BinaryTree_Pruning {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+        if (root.val == 0 && root.left == null && root.right == null)
+            root = null;
+        return root;
+    }
+}
