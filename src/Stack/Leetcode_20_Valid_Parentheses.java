bab3f858c76fc7d78c6087728121fe6b93d54c61
saiful00000 <>
2018-11-23 02:50:34 +0600

    not accepted

---
+++ src/Stack/Leetcode_20_Valid_Parentheses.java
@@ -0,0 +1,33 @@
+package Stack;
+
+import java.util.Stack;
+
+public class Leetcode_20_Valid_Parentheses {
+
+    public static boolean isValid(String parentheses) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < parentheses.length()-1; i++) {
+            char c = parentheses.charAt(i);
+            if (c == '(' || c == '{' || c == '[')
+                stack.push(c);
+            else
+                if (c == ')' && stack.peek() == '(')
+                    stack.pop();
+                else if (c == '}' && stack.peek() == '{')
+                    stack.pop();
+                else if (c == ']' && stack.peek() == '[')
+                    stack.pop();
+                else
+                    return false;
+        }
+        if (stack.isEmpty())
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        String s = "()[]{}]";
+        System.out.println(isValid(s));
+    }
+}
