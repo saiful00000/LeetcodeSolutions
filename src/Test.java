109d544d99b6eed4e161500e61c7202a6ec7268b
saiful00000 <>
2018-11-24 20:20:41 +0600

    accepted 100% best solution

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,7 @@ public class Test {
 
 
     public static void main(String[] args) {
-        System.out.println(10*(-1));
+        char c = 'A' + 32;
+        System.out.println(c);
     }
-
-
 }
---
+++ src/strings/Leetcode_709_To_Lower_Case.java
@@ -0,0 +1,7 @@
+package strings;
+
+public class Leetcode_709_To_Lower_Case {
+    public String toLowerCase(String string) {
+        return string.toLowerCase();
+    }
+}
