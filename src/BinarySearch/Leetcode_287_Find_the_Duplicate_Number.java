c19f8a86e8ceb18f90b0635d53c8c8577962fd66
saiful00000 <>
2018-11-27 13:47:08 +0600

    accepted

---
+++ src/BinarySearch/Leetcode_287_Find_the_Duplicate_Number.java
@@ -0,0 +1,19 @@
+package BinarySearch;
+
+import java.util.Arrays;
+
+public class Leetcode_287_Find_the_Duplicate_Number {
+    public static int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i+1])
+                return nums[i];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,2,2,2,2};
+        System.out.println(findDuplicate(nums));
+    }
+}
